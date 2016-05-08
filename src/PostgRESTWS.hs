3c82b756d39169f354e98ec966e95f34ce470606
Diogo Biazus <diogob@gmail.com>
2016-05-08 11:03:06 -0400

    Show client headers so we can start working on the authentication

--- src/PostgRESTWS.hs
+++ src/PostgRESTWS.hs
@@ -2,7 +2,6 @@ module PostgRESTWS
   ( postgrestWsApp
   ) where
 
-import qualified Data.Text as T
 import qualified Network.Wai                     as Wai
 import qualified Network.WebSockets              as WS
 import qualified Network.Wai.Handler.WebSockets  as WS
@@ -11,14 +10,17 @@ import           PostgREST.Config as PGR
 import           PostgREST.Types  as PGR
 import qualified Hasql.Pool as H
 import GHC.IORef
+import Data.Monoid
+import qualified Data.Text as T
 
 postgrestWsApp :: PGR.AppConfig
                     -> IORef PGR.DbStructure
                     -> H.Pool
                     -> Wai.Application
-postgrestWsApp conf refDbStructure pool = WS.websocketsOr WS.defaultConnectionOptions wsApp $ postgrest conf refDbStructure pool
+postgrestWsApp conf refDbStructure pool =
+  WS.websocketsOr WS.defaultConnectionOptions wsApp $ postgrest conf refDbStructure pool
   where
     wsApp :: WS.ServerApp
     wsApp pending_conn = do
       conn <- WS.acceptRequest pending_conn
-      WS.sendTextData conn ("Hello, client!" :: T.Text)
+      WS.sendTextData conn ("Hello, client! Your headers are: " <> T.pack (show $ WS.requestHeaders (WS.pendingRequest pending_conn)))
