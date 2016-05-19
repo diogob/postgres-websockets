6b85dfa6c2c564de1041d0512ca1e3ee2a0bc0cf
Diogo Biazus <diogob@gmail.com>
2016-05-18 23:36:11 -0400

    Put basic types in place to create a session recorder and a player

--- src/PostgRESTWS.hs
+++ src/PostgRESTWS.hs
@@ -12,6 +12,8 @@ import qualified Hasql.Pool as H
 import GHC.IORef
 import Data.Monoid
 import qualified Data.Text as T
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
 
 postgrestWsApp :: PGR.AppConfig
                     -> IORef PGR.DbStructure
@@ -21,6 +23,19 @@ postgrestWsApp conf refDbStructure pool =
   WS.websocketsOr WS.defaultConnectionOptions wsApp $ postgrest conf refDbStructure pool
   where
     wsApp :: WS.ServerApp
-    wsApp pending_conn = do
-      conn <- WS.acceptRequest pending_conn
-      WS.sendTextData conn ("Hello, client! Your headers are: " <> T.pack (show $ WS.requestHeaders (WS.pendingRequest pending_conn)))
+    wsApp pendingConn = do
+      -- We should accept only after verifying JWT
+      conn <- WS.acceptRequest pendingConn
+      forever $ sessionHandler conn
+      where
+        sessionHandler = chooseSession headers
+        headers = WS.requestHeaders $ WS.pendingRequest pendingConn
+
+sessionRecorder :: WS.Connection -> IO ()
+sessionRecorder = undefined
+
+sessionPlayer :: WS.Connection -> IO ()
+sessionPlayer = undefined
+
+chooseSession :: WS.Headers -> WS.Connection -> IO ()
+chooseSession = undefined
