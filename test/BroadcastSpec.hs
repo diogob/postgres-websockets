b037a34c4aa03d3acecc3ff30d6da98f528e40ae
Diogo Biazus <diogob@nulogy.com>
2017-04-07 08:39:24 -0400

    Add new test case for newMultiplexer

--- test/BroadcastSpec.hs
+++ test/BroadcastSpec.hs
@@ -9,8 +9,17 @@ import Test.Hspec
 import PostgRESTWS.Broadcast
 
 spec :: Spec
-spec =
-  describe "Broadcast" $
+spec = do
+  describe "newMultiplexer" $
+    it "opens a separate thread for a producer function" $ do
+      output <- newTQueueIO :: IO (TQueue ThreadId)
+      void $ liftIO $ newMultiplexer (\_ _-> do
+        tid <- myThreadId
+        atomically $ writeTQueue output tid
+        ) (\_ -> return ())
+      outMsg <- atomically $ readTQueue output
+      myThreadId `shouldNotReturn` outMsg
+  describe "relayMessages" $
     it "relays a single message from producer to 1 listener on 1 test channel" $ do
       output <- newTQueueIO :: IO (TQueue Message)
       multi <- liftIO $ newMultiplexer (\_ msgs-> atomically $ writeTQueue msgs (Message "test" "payload")) (\_ -> return ())
