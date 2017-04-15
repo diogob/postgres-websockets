5e20e94a4d4182401d1f00f9ea09b384f663865e
Diogo Biazus <diogo@biazus.me>
2017-04-14 20:38:53 -0400

    First draft code for using the multiplexer [#9]

--- src/PostgRESTWS.hs
+++ src/PostgRESTWS.hs
@@ -19,25 +19,28 @@ import qualified Data.ByteString.Lazy           as BL
 
 import PostgRESTWS.Claims
 import PostgRESTWS.Database
+import PostgRESTWS.Broadcast (Multiplexer, onMessage, readTChan)
 
 data Message = Message A.Object Text deriving (Show, Eq, Generic)
 
 instance A.ToJSON Message
 
-postgrestWsMiddleware :: Text -> Maybe ByteString -> IO POSIXTime -> H.Pool -> Wai.Application -> Wai.Application
+postgrestWsMiddleware :: Maybe ByteString -> IO POSIXTime -> H.Pool -> Multiplexer -> Wai.Application -> Wai.Application
 postgrestWsMiddleware = WS.websocketsOr WS.defaultConnectionOptions `compose` wsApp
   where
     compose = (.) . (.) . (.) . (.)
 
 -- when the websocket is closed a ConnectionClosed Exception is triggered
 -- this kills all children and frees resources for us
-wsApp :: Text -> Maybe ByteString -> IO POSIXTime -> H.Pool -> WS.ServerApp
-wsApp pgSettings mSecret getTime pqCon pendingConn = getTime >>= forkSessionsWhenTokenIsValid . validateClaims mSecret jwtToken
+wsApp :: Maybe ByteString -> IO POSIXTime -> H.Pool -> Multiplexer -> WS.ServerApp
+wsApp mSecret getTime pqCon multi pendingConn = getTime >>= forkSessionsWhenTokenIsValid . validateClaims mSecret jwtToken
   where
     forkSessionsWhenTokenIsValid = either rejectRequest forkSessions
     hasRead m = m == ("r" :: ByteString) || m == ("rw" :: ByteString)
     hasWrite m = m == ("w" :: ByteString) || m == ("rw" :: ByteString)
     rejectRequest = WS.rejectRequest pendingConn . encodeUtf8
+    toBS :: Show a => a -> ByteString
+    toBS = show
     -- the first char in path is '/' the rest is the token
     jwtToken = decodeUtf8 $ BS.drop 1 $ WS.requestPath $ WS.pendingRequest pendingConn
     forkSessions (channel, mode, claims) = do
@@ -48,14 +51,14 @@ wsApp pgSettings mSecret getTime pqCon pendingConn = getTime >>= forkSessionsWhe
           WS.forkPingThread conn 30
           -- each websocket needs its own listen connection to avoid
           -- handling of multiple waiting threads in the same connection
-          listenSessionFinished <- if hasRead mode
-            then forkAndWait $ listenSession channel (toS pgSettings) conn
-            else newMVar ()
+
+          when (hasRead mode) $
+            onMessage multi channel (\ch ->
+              (atomically $ readTChan ch) >>= WS.sendTextData conn . toBS)
           -- all websockets share a single connection to NOTIFY
           notifySessionFinished <- if hasWrite mode
             then forkAndWait $ forever $ notifySession channel claims pqCon conn
             else newMVar ()
-          takeMVar listenSessionFinished
           takeMVar notifySessionFinished
 
 -- private functions
@@ -74,12 +77,6 @@ notifySession channel claims pool wsCon =
     send = notify pool channel
     -- we need to decode the bytestring to re-encode valid JSON for the notification
     jsonMsg = BL.toStrict . A.encode . Message claims . decodeUtf8With T.lenientDecode
-
-listenSession :: ByteString
-                    -> ByteString
-                    -> WS.Connection
-                    -> IO ()
-listenSession channel pgSettings wsCon = onNotification channel pgSettings $ WS.sendTextData wsCon
 
 forkAndWait :: IO () -> IO (MVar ())
 forkAndWait io = do
