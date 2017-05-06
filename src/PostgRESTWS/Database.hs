14948189ff6c2de9b6884881c64bff8fa5e1f962
Diogo Biazus <diogo@biazus.me>
2017-05-06 13:44:39 -0400

    Add haddock comments to Database module

--- src/PostgRESTWS/Database.hs
+++ src/PostgRESTWS/Database.hs
@@ -1,3 +1,8 @@
+{-|
+Module      : PostgRESTWS.Database
+Description : This module encapsulates knowledge about the SQL commands and the Hasql interface.
+
+-}
 module PostgRESTWS.Database
   ( notifyPool
   , notify
@@ -16,6 +21,7 @@ import Data.Either.Combinators
 
 newtype Error = NotifyError Text
 
+-- | Given a Hasql Pool, a channel and a message sends a notify command to the database
 notifyPool :: Pool -> ByteString -> ByteString -> IO (Either Error ())
 notifyPool pool channel mesg =
    mapError <$> use pool (sql ("NOTIFY " <> channel <> ", '" <> mesg <> "'"))
@@ -23,6 +29,7 @@ notifyPool pool channel mesg =
      mapError :: Either UsageError () -> Either Error ()
      mapError = mapLeft (NotifyError . show)
 
+-- | Given a Hasql Connection, a channel and a message sends a notify command to the database
 notify :: Connection -> ByteString -> ByteString -> IO (Either Error ())
 notify con channel mesg =
    mapError <$> run (sql ("NOTIFY " <> channel <> ", '" <> mesg <> "'")) con
@@ -30,18 +37,25 @@ notify con channel mesg =
      mapError :: Either S.Error () -> Either Error ()
      mapError = mapLeft (NotifyError . show)
 
+-- | Given a Hasql Connection and a channel sends a listen command to the database
 listen :: Connection -> ByteString -> IO ()
 listen con channel =
   void $ withLibPQConnection con execListen
   where
     execListen pqCon = void $ PQ.exec pqCon $ "LISTEN " <> channel
 
+-- | Given a Hasql Connection and a channel sends a unlisten command to the database
 unlisten :: Connection -> ByteString -> IO ()
 unlisten con channel =
   void $ withLibPQConnection con execListen
   where
     execListen pqCon = void $ PQ.exec pqCon $ "UNLISTEN " <> channel
 
+
+{- | Given a function that handles notifications and a Hasql connection forks a thread that listens on the database connection and calls the handler everytime a message arrives.
+
+   The message handler passed as first argument needs two parameters channel and payload.
+-}
 
 waitForNotifications :: (ByteString -> ByteString -> IO()) -> Connection -> IO ()
 waitForNotifications sendNotification con =
