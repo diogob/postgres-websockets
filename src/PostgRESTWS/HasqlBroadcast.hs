6d2a7cb0593ea00a711359e34f1bf2c974f53ccb
Diogo Biazus <diogo@biazus.me>
2017-05-06 13:23:40 -0400

    Add haddock comments to give example of database multiplexer.

--- src/PostgRESTWS/HasqlBroadcast.hs
+++ src/PostgRESTWS/HasqlBroadcast.hs
@@ -1,3 +1,11 @@
+{-|
+Module      : PostgRESTWS.HasqlBroadcast
+Description : Uses Broadcast module adding database as a source producer.
+
+This module provides a function to produce a 'Multiplexer' from a Hasql 'Connection'.
+The producer issues a LISTEN command upon Open commands and UNLISTEN upon Close.
+
+-}
 module PostgRESTWS.HasqlBroadcast
   ( newHasqlBroadcaster
   -- re-export
@@ -13,6 +21,26 @@ import Hasql.Connection
 import PostgRESTWS.Database
 import PostgRESTWS.Broadcast
 
+{- | Returns a multiplexer from a connection, listen for different database notification channels using that connection.
+
+   To listen on channels *chat*
+
+   @
+   import Protolude
+   import PostgRESTWS.HasqlBroadcast
+   import PostgRESTWS.Broadcast
+   import Hasql.Connection
+
+   main = do
+    conOrError <- H.acquire "postgres://localhost/test_database"
+    let con = either (panic . show) id conOrError :: Connection
+    multi <- newHasqlBroadcaster con
+
+    onMessage multi "chat" (\ch ->
+      forever $ fmap print (atomically $ readTChan ch)
+   @
+
+-}
 newHasqlBroadcaster :: Connection -> IO Multiplexer
 newHasqlBroadcaster con = newMultiplexer (\cmds msgs-> do
     waitForNotifications
