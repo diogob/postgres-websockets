7129495ffa8083c96ca09d51c4dc28ed575594dc
Pierre Radermecker <pierrer@pi3r.be>
2017-10-16 11:47:55 +0200

    Add ClaimSpec with a simple test case

---
+++ test/ClaimsSpec.hs
@@ -0,0 +1,17 @@
+module ClaimsSpec (spec) where
+
+import           Protolude
+
+import qualified Data.HashMap.Strict as M
+import           Test.Hspec
+import           Data.Aeson          (Value (..) )
+
+import           PostgRESTWS.Claims
+
+spec :: Spec
+spec =
+  describe "validate claims"
+  $ it "should succeed using a matching token"
+  $ validateClaims "reallyreallyreallyreallyverysafe"
+                   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoiciIsImNoYW5uZWwiOiJ0ZXN0In0.1d4s-at2kWj8OSabHZHTbNh1dENF7NWy_r0ED3Rwf58"
+                   `shouldReturn` Right ("test", "r", M.fromList[("mode",String "r"),("channel",String "test")])
