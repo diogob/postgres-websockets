bbd742fdbbd81188b4135f2ea2aa4094bde5fa8d
James Stone <jamesmstone@users.noreply.github.com>
2020-07-23 11:17:50 +1000

    add some untested tests, (as I haven't been able to get the main program to compile ~sigh~

--- test/ClaimsSpec.hs
+++ test/ClaimsSpec.hs
@@ -20,4 +20,13 @@ spec =
       time <- getCurrentTime
       validateClaims Nothing "reallyreallyreallyreallyverysafe"
                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoiciIsImNoYW5uZWwiOiJ0ZXN0In0.1d4s-at2kWj8OSabHZHTbNh1dENF7NWy_r0ED3Rwf58" time
-                   `shouldReturn` Right ("test", "r", M.fromList[("mode",String "r"),("channel",String "test")])
+                   `shouldReturn` Right (["test"], "r", M.fromList[("mode",String "r"),("channels",String "test")])    it "should succeed using a matching token" $ do
+      time <- getCurrentTime
+      validateClaims Nothing "reallyreallyreallyreallyverysafe"
+                   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoiciIsImNoYW5uZWxzIjpbInRlc3QiXSwiZXhwIjoxfQ.jPPRtgz_TSk1Ft5b1JEdwF28yi790wcghePbpLORcBM" time
+                   `shouldReturn` Right (["test"], "r", M.fromList[("mode",String "r"),("channels",M.fromList[ "test")])
+
+      time <- getCurrentTime
+      validateClaims Nothing "reallyreallyreallyreallyverysafe"
+                   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoiciIsImNoYW5uZWxzIjpbInRlc3QiXSwiZXhwIjoxfQ.jPPRtgz_TSk1Ft5b1JEdwF28yi790wcghePbpLORcBM" time
+                   `shouldReturn` Right ("test":"test2":[], "r", M.fromList[("mode",String "r"),("channels",M.fromList[ "test", "test2")])
