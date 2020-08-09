8533f439472f1135371c281594e5304c3f0d2005
Diogo Biazus <diogo.biazus@coinberry.com>
2020-08-09 18:09:04 -0400

    Add receive message test

--- test/ServerSpec.hs
+++ test/ServerSpec.hs
@@ -10,6 +10,9 @@ import qualified Hasql.Connection as Hasql
 import qualified Hasql.Session as Hasql
 import qualified Hasql.Statement as Hasql
 
+import Control.Lens
+import Data.Aeson.Lens
+
 import qualified Network.WebSockets as WS
 import           Network.Socket (withSocketsDo)
 
@@ -43,8 +46,29 @@ sendWsData msg =
             "/test/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoicncifQ.auy9z4-pqoVEAay9oMi1FuG7ux_C_9RQCH8-wZgej18" 
             (`WS.sendTextData` msg)
 
+waitForWsData :: Text -> IO (MVar ByteString)
+waitForWsData channel = do
+    msg <- newEmptyMVar
+    void $ forkIO $
+        withSocketsDo $ 
+            WS.runClient 
+                "localhost" 
+                (configPort testServerConfig) 
+                "/test/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoicncifQ.auy9z4-pqoVEAay9oMi1FuG7ux_C_9RQCH8-wZgej18" 
+                (\c -> do
+                    m <- WS.receiveData c
+                    putMVar msg m
+                )
+    pure msg
+
 spec :: Spec
 spec = around_ withServer $
-            describe "serve" $
+            describe "serve" $ do
                 it "should be able to send messages to test server" $
                     sendWsData "test data"
+                it "should be able to receive messages from test server" $ do
+                    msg <- waitForWsData "test"
+                    threadDelay 1000
+                    sendWsData "test data"
+                    msgJson <- takeMVar msg
+                    (msgJson ^? key "payload" . _String) `shouldBe` Just "test data"
