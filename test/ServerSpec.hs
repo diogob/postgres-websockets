31b8807030d9679c08f7dd7d933985e2c038078f
Diogo Biazus <diogo.biazus@coinberry.com>
2020-08-09 17:21:04 -0400

    Add ServerSpec to perform end-to-end testing using a websockets client

---
+++ test/ServerSpec.hs
@@ -0,0 +1,51 @@
+module ServerSpec (spec) where
+
+import Protolude
+
+import Data.Function (id)
+import Test.Hspec
+import PostgresWebsockets
+
+import qualified Hasql.Connection as Hasql
+import qualified Hasql.Session as Hasql
+import qualified Hasql.Statement as Hasql
+
+import qualified Network.WebSockets as WS
+import           Network.Socket (withSocketsDo)
+
+testServerConfig :: AppConfig
+testServerConfig = AppConfig 
+                    { configDatabase = "postgres://localhost/postgres"
+                    , configPath = Nothing
+                    , configHost = "*"
+                    , configPort = 8080
+                    , configListenChannel = "postgres-websockets-test-channel"
+                    , configJwtSecret = "reallyreallyreallyreallyverysafe"
+                    , configJwtSecretIsBase64 = False
+                    , configPool = 10
+                    }
+
+startTestServer :: IO ThreadId
+startTestServer = do
+    forkIO $ serve testServerConfig
+
+withServer :: IO () -> IO ()
+withServer action =
+  bracket startTestServer
+          killThread
+          (const action)
+
+sendWsData :: Text -> IO ()
+sendWsData msg = do
+    withSocketsDo $ 
+        WS.runClient 
+            "localhost" 
+            (configPort testServerConfig) 
+            "/test/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoicncifQ.auy9z4-pqoVEAay9oMi1FuG7ux_C_9RQCH8-wZgej18" 
+            (flip WS.sendTextData msg)
+
+spec :: Spec
+spec = around_ withServer $ do
+            describe "serve" $
+                it "should be able to send messages to test server" $ do
+                    sendWsData "test data"
