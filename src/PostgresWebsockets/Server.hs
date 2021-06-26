f6518f1cd6e5f103ddbaa3b0280922a5ca9c766f
Diogo Biazus <diogo@biazus.ca>
2021-06-26 11:00:29 -0400

    Just extract app definition for code clarity and to make TLS introduction easier

--- src/PostgresWebsockets/Server.hs
+++ src/PostgresWebsockets/Server.hs
@@ -1,42 +1,39 @@
-{-|
-Module      : PostgresWebsockets.Server
-Description : Functions to start a full stand-alone PostgresWebsockets server.
--}
+-- |
+-- Module      : PostgresWebsockets.Server
+-- Description : Functions to start a full stand-alone PostgresWebsockets server.
 module PostgresWebsockets.Server
-        ( serve 
-        ) where
+  ( serve,
+  )
+where
 
-import Protolude
-import Network.Wai.Application.Static ( staticApp, defaultFileServerSettings )
-import Network.Wai (Application, responseLBS)
 import Network.HTTP.Types (status200)
-import Network.Wai.Handler.Warp ( runSettings )
+import Network.Wai (Application, responseLBS)
+import Network.Wai.Application.Static (defaultFileServerSettings, staticApp)
+import Network.Wai.Handler.Warp (runSettings)
 import Network.Wai.Middleware.RequestLogger (logStdout)
-
-import PostgresWebsockets.Middleware ( postgresWsMiddleware )
-import PostgresWebsockets.Config ( AppConfig(..), warpSettings )
-import PostgresWebsockets.Context ( mkContext )
+import PostgresWebsockets.Config (AppConfig (..), warpSettings)
+import PostgresWebsockets.Context (mkContext)
+import PostgresWebsockets.Middleware (postgresWsMiddleware)
+import Protolude
 
 -- | Start a stand-alone warp server using the parameters from AppConfig and a opening a database connection pool.
 serve :: AppConfig -> IO ()
-serve conf@AppConfig{..} = do
+serve conf@AppConfig {..} = do
   shutdownSignal <- newEmptyMVar
-  let waitForShutdown cl = void $ forkIO (takeMVar shutdownSignal >> cl)
-      appSettings = warpSettings waitForShutdown conf
-
   putStrLn $ ("Listening on port " :: Text) <> show configPort
 
   let shutdown = putErrLn ("Broadcaster connection is dead" :: Text) >> putMVar shutdownSignal ()
   ctx <- mkContext conf shutdown
 
-  runSettings appSettings $
-    postgresWsMiddleware ctx $
-    logStdout $ maybe dummyApp staticApp' configPath
-  die "Shutting down server..."
+  let waitForShutdown cl = void $ forkIO (takeMVar shutdownSignal >> cl)
+      appSettings = warpSettings waitForShutdown conf
+      app = postgresWsMiddleware ctx $ logStdout $ maybe dummyApp staticApp' configPath
 
+  runSettings appSettings app
+  die "Shutting down server..."
   where
     staticApp' :: Text -> Application
     staticApp' = staticApp . defaultFileServerSettings . toS
     dummyApp :: Application
     dummyApp _ respond =
-        respond $ responseLBS status200 [("Content-Type", "text/plain")] "Hello, Web!"
+      respond $ responseLBS status200 [("Content-Type", "text/plain")] "Hello, Web!"
