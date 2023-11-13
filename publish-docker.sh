da97c91a66f12f31971f7c040710a232f802d319
Diogo Biazus <diogo@biazus.ca>
2023-11-13 09:37:03 -0500

    Add cross architecture build commends to publish-docker.sh scripts

--- publish-docker.sh
+++ publish-docker.sh
@@ -1,5 +1,5 @@
 VERSION=$(awk '/^version: / { print $2 };' < postgres-websockets.cabal)
-docker build . --tag diogob/postgres-websockets:latest
-docker tag diogob/postgres-websockets:latest diogob/postgres-websockets:$VERSION
-docker push diogob/postgres-websockets
-docker push diogob/postgres-websockets:$VERSION
+docker buildx build --platform amd64 .
+docker buildx build --platform arm64 .
+docker buildx build -t diogob/postgres-websockets:latest --platform arm64,amd64 --push .
+docker buildx build -t diogob/postgres-websockets:$VERSION --platform arm64,amd64 --push .
