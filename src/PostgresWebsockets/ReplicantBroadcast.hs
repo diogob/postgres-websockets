356b96ffe4337a380025f7908bcc7a0440899e4b
Diogo Biazus <diogo@biazus.ca>
2024-12-15 16:04:12 -0500

    Experimenting with sending arbitrary queries upon change

--- src/PostgresWebsockets/ReplicantBroadcast.hs
+++ src/PostgresWebsockets/ReplicantBroadcast.hs
@@ -14,13 +14,18 @@ where
 
 import Data.Aeson (encode)
 import qualified Database.PostgreSQL.Replicant as PGR
+import qualified Hasql.Decoders as Decode
+import qualified Hasql.Encoders as SQL
+import qualified Hasql.Pool as SQL
+import qualified Hasql.Session as SQL
+import qualified Hasql.Statement as SQL
 import PostgresWebsockets.Broadcast
 import Protolude hiding (putErrLn, show, toS)
 import Protolude.Conv
 
 -- | Returns a multiplexer from a connection URI, keeps trying to connect in case there is any error.
 --   This function also spawns a thread that keeps relaying the messages from the database to the multiplexer's listeners
-newReplicantBroadcaster :: IO () -> Int -> Maybe Int -> ByteString -> IO Multiplexer
+newReplicantBroadcaster :: IO () -> Int -> Maybe Int -> SQL.Pool -> ByteString -> IO Multiplexer
 newReplicantBroadcaster onConnectionFailure _maxRetries = newReplicantBroadcasterForChanges onConnectionFailure
 
 --
@@ -45,8 +50,8 @@ newReplicantBroadcaster onConnectionFailure _maxRetries = newReplicantBroadcaste
 --    onMessage multi "chat" (\ch ->
 --      forever $ fmap print (atomically $ readTChan ch)
 --   @
-newReplicantBroadcasterForChanges :: IO () -> Maybe Int -> ByteString -> IO Multiplexer
-newReplicantBroadcasterForChanges onConnectionFailure checkInterval _conURI = do
+newReplicantBroadcasterForChanges :: IO () -> Maybe Int -> SQL.Pool -> ByteString -> IO Multiplexer
+newReplicantBroadcasterForChanges onConnectionFailure checkInterval pool _conURI = do
   multi <- newMultiplexer openProducer $ const onConnectionFailure
   case checkInterval of
     Just i -> superviseMultiplexer multi i shouldRestart
@@ -75,12 +80,18 @@ newReplicantBroadcasterForChanges onConnectionFailure checkInterval _conURI = do
           PGR.withLogicalStream settings $ \changePayload -> do
             print ("Change received!" :: Text)
             print $ encode changePayload
-            writeMessage $ toS $ encode changePayload
+            result <- SQL.use pool session
+            case result of
+              Right value -> writeMessage $ toS $ encode value
+              Left _ -> print ("ERROR executing" :: Text)
             case changePayload of
               PGR.InformationMessage _infoMsg ->
                 pure Nothing
               PGR.ChangeMessage change ->
                 pure . Just $ PGR.changeNextLSN change
       where
-        writeMessage m = atomically $ writeTQueue msgQ $ toMsg m
+        session = SQL.statement () stmt
+        stmt = SQL.Statement "SELECT json_agg(row_to_json(t.*)) FROM test t" SQL.noParams singleJsonDecoder True
+        singleJsonDecoder = Decode.singleRow $ Decode.column $ Decode.nonNullable Decode.json
 
+        writeMessage m = atomically $ writeTQueue msgQ $ toMsg m
