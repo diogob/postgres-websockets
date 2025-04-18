3bb1028c289e8fa19ffe1027839aecaab8128744
Diogo Biazus <diogo@biazus.ca>
2025-04-18 16:46:11 -0400

    Remove protolude since the library seems a bit stale and we can replace the interesting bits with a simpler module.

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,19 @@
 module Main where
 
-import Protolude
+import APrelude
 import PostgresWebsockets
-
 import System.IO (BufferMode (..), hSetBuffering)
 
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
-  hSetBuffering stdin  LineBuffering
+  hSetBuffering stdin LineBuffering
   hSetBuffering stderr NoBuffering
 
-  putStrLn $ ("postgres-websockets " :: Text)
-               <> prettyVersion
-               <> " / Connects websockets to PostgreSQL asynchronous notifications."
+  putStrLn $
+    ("postgres-websockets ")
+      <> (unpack prettyVersion)
+      <> " / Connects websockets to PostgreSQL asynchronous notifications."
 
   conf <- loadConfig
   void $ serve conf
